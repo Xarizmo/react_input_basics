fbca19d897e818140c3e354345ddee42452919b5
Mikhail Polikarpov <mpolikarpoff@gmail.com>
2020-04-12 00:52:25 +0300

    add functionality

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import React from 'react';
+import React from "react";
+import "./App.css";
 
 export default function App() {
-  
-  const [ draft, setDraft ] = React.useState("");
+	const [draft, setDraft] = React.useState("");
 
-  return (
-      <div>
-        <input 
-          type="text" 
-          value={draft}
-          onChange={(event) => setDraft(event.target.value)}
-        />
-        <br />
-        <button 
-            onClick={() => alert(draft)}>
-          Show input value
-        </button>
-      </div>
-  );
+	return (
+		<div className="input-app">
+			<span>Вы ввели: {draft}</span>
+			<input
+				type="text"
+				className="form-control"
+				value={draft}
+				onChange={event => setDraft(event.target.value)}
+				autoFocus
+			/>
+			<br />
+			<button
+				type="submit"
+				className="btn btn-primary"
+				onClick={() => {
+					alert(draft);
+					setDraft("");
+				}}
+			>
+				Show input value
+			</button>
+		</div>
+	);
 }
